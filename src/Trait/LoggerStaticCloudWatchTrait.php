3a12c19a0b8a3204d349afb46c4b043b29d1f809
Ivan Gerardo Balam Abraham <ivbalam@palace-resorts.local>
2022-04-12 09:18:07 -0500

    ADDED: Static trait for cloudwatch service

---
+++ src/Trait/LoggerStaticCloudWatchTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PrAWSLogger\Tr;
+
+use PrAWSLogger\CloudWatchLogService;
+
+trait LoggerStaticCloudWatchTrait
+{
+    private static $track_uuid;
+    private static $service;
+
+    public static function initLogger($track_uuid = '', $instance = null, $group = null, $retencion = 90) {
+        self::$service = new CloudWatchLogService();
+        self::$service->initLogger($track_uuid, $instance, $group, $retencion);
+        self::$track_uuid = self::$service->track_uuid;
+    }
+
+    public static function errorCurl($instance, $message, $code, $request, $system = null){
+        self::$service->errorCurl($instance, $message, $code, $request, $system);
+    }
+
+    public static function warn($instance, $message, $code, $request, $system = null){
+        self::$service->warn($instance, $message, $code, $request, $system);
+    }
+
+    public static function setWarning($data, $request, $system = null){
+        self::$service->setWarning($data, $request, $system);
+    }
+
+    public static function setError($data, $request, $system = null) {
+        self::$service->setError($data, $request, $system);
+    }
+}
